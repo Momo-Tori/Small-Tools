f5cd9c449fb7a31168dd4bd6c78b1a21337ca82c
MomoTori <813424850@qq.com>
2022-11-03 13:15:06 +0800

    feat: add support to Windows, and add multy-inputs

--- 运行脚本/run.py
+++ 运行脚本/run.py
@@ -1,33 +1,69 @@
 #!/usr/bin/env python3
 import os
+import sys
 import subprocess
 
+
 exDir = "ex"
+inputDir = "input"
+tempDir = "temp"
 Timeout = 0.5
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+inFileMap = {}
+for iF in os.listdir(inputDir):
+    index = iF.find('-')
+    point = iF.find('.')
+    if index == -1:
+        inFileMap[iF[:point]] = {"": iF}
+    else:
+        if iF[:index] in inFileMap:
+            inFileMap[iF[:index]][iF[index+1:point]] = iF
+        else:
+            inFileMap[iF[:index]] = {iF[index+1:point]: iF}
+
+if not os.path.exists(tempDir):
+    os.makedirs(tempDir)
 
 ex = os.listdir(exDir)
 
-if not os.path.exists("temp"):
-    os.system("mkdir temp")
-
 for student in ex:
-    sourceDir = exDir+"/"+student
+    sourceDir = os.path.join(exDir, student)
     sourceFile = os.listdir(sourceDir)
     for sF in sourceFile:
-        if not (sF[sF.rfind("."):] in ('.cpp','.c')):
+        if not (sF[sF.rfind("."):] in ('.cpp', '.c')):
             continue
         objName = sF[:sF.rfind(".")]
-        path = sourceDir + "/" + sF
-        os.system("gcc "+'"'+path+'"'+" -o "+'"temp/'+objName+'"')
+        path = os.path.join(sourceDir, sF)
+        subprocess.run(
+            ["gcc", path,
+             "-O2",
+             "-o", os.path.join(tempDir, objName+".out")])
 
-        if os.path.exists("input/"+objName+".txt"):
-            inFile = open("input/"+objName+".txt","r")
-        else:
-            inFile = None
-        outFile = open(sourceDir+'/'+objName+'.txt',"w")
-        try:
-            subprocess.run(['temp/'+objName],stdin=inFile,stdout=outFile,timeout=Timeout)
-        except:
-            print(student+':'+sF+" Timeout!")
+        inFileList = inFileMap.get(objName, {"": ""})
 
-ex = os.system("rm temp -r")
+        for key, value in inFileList.items():
+            if key == "":
+                outFile = open(os.path.join(sourceDir, objName+'.txt'), "w")
+            else:
+                outFile = open(os.path.join(
+                    sourceDir, objName+'-'+key+'.txt'), "w")
+
+            if value == "":
+                inFile = None
+            else:
+                inFile = open(os.path.join(inputDir, value), "r")
+
+            try:
+                subprocess.run(
+                    [os.path.join('temp', objName+".out")],
+                    stdin=inFile,
+                    stdout=outFile,
+                    timeout=Timeout)
+            except:
+                print(student+': '+sF+" Running Timeout!")
+
+for file in os.listdir(tempDir):
+    os.remove(os.path.join(tempDir, file))
+os.rmdir(tempDir)
