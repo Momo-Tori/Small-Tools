2e6d445a59f4b5ee7c83cac16dade1b5deb0e48f
MomoTori <813424850@qq.com>
2022-12-03 13:26:52 +0800

    style: remove sudo but need run in su mode

--- save/quick-setup.sh
+++ save/quick-setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+# !!!!!!!请在su下运行该脚本!!!!!!!
+
 # APT config first
 SOURCES="/etc/apt/sources.list"
-sudo sed -Ei 's/([a-z]*\.)?archive\.ubuntu\.com/mirrors.ustc.edu.cn/g' "$SOURCES"
-sudo sed -Ei 's/security\.ubuntu\.com/mirrors.ustc.edu.cn/g' "$SOURCES"
-sudo sed -Ei 's/http:/https:/g' "$SOURCES"
+sed -Ei 's/([a-z]*\.)?archive\.ubuntu\.com/mirrors.ustc.edu.cn/g' "$SOURCES"
+sed -Ei 's/security\.ubuntu\.com/mirrors.ustc.edu.cn/g' "$SOURCES"
+sed -Ei 's/http:/https:/g' "$SOURCES"
 
 # APT Packages
 export DEBIAN_FRONTEND=noninteractive
-sudo apt update
-sudo apt -y full-upgrade
-sudo apt -y install vim build-essential python3-pip git openssh-client wget curl tldr tree cmake gdb tmux nodejs npm zsh zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel9k
+apt update
+apt -y full-upgrade
+apt -y install vim build-essential python3-pip git openssh-client wget curl tldr tree cmake gdb tmux nodejs npm zsh zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel9k
 unset DEBIAN_FRONTEND
 
 chsh -s /usr/bin/zsh
@@ -19,11 +21,11 @@ chsh -s /usr/bin/zsh
 
 # 首先升级node
 
-sudo npm install -g n
-sudo n latest
-sudo n prune
+npm install -g n
+n latest
+n prune
 
-sudo npm install -g commitizen
+npm install -g commitizen
 echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
 
 # 迁移 .zshrc
